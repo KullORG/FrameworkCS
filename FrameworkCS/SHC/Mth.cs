42043a9d35ee257538c54c9d098944667e04996c
Projectmaster122 <68382320+Projectmaster122@users.noreply.github.com>
2021-12-06 16:08:38 +0200

    Add sin and cos to Mth.cs

--- FrameworkCS/SHC/Mth.cs
+++ FrameworkCS/SHC/Mth.cs
@@ -8,6 +8,16 @@ namespace SHC
 {
     public class Mth
     {
-        // 
+        // Math.Round shorter. Mth.Rnd
+        public static double Rnd(double num)
+        {
+            return Math.Round(num);
+            
+        }
+        // Math.Sin Mth.Sin
+        public static double Sin(double num) { return Math.Sin(num); }
+        // Math.Cos Mth.Cos
+        public static double Cos(double num) { return Math.Cos(num); }
+        
     }
 }
