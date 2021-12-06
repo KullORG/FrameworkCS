f4c191672cb5dee09729ce3d75712dce066ee275
Projectmaster122 <68382320+Projectmaster122@users.noreply.github.com>
2021-12-06 19:02:42 +0200

    Added Fun.cs with some classes. Use when YandreDev

---
+++ FrameworkCS/SHC/Fun.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHC
+{
+    public class HellaRandom
+    {
+        public static int one = 1;
+        public static int two = 2;
+        public static int three = 3;
+        public static int four = 4;
+        public static int five = 5;
+        public static int six = 6;
+        public static int seven = 7;
+        public static int eight = 8;
+        public static int nine = 9;
+        public static int ten = 10;
+        public static int eleven = 11;
+        public static int twelve = 12;
+        public static int thirteen = 13;
+        public static int fourteen = 14;
+        public static int fifteen = 15;
+        public static int sixteen = 16;
+        public static int seventeen = 17;
+        public static int eightteen = 18;
+        public static int nineteen = 19;
+        public static int twenty = 20;
+    }
+    public class COLORS
+    {
+        public static ConsoleColor red = ConsoleColor.Red;
+        public static ConsoleColor green = ConsoleColor.Green;
+        public static ConsoleColor blue = ConsoleColor.Blue;
+        public static ConsoleColor white = ConsoleColor.White;
+        public static ConsoleColor black = ConsoleColor.Black;
+        public static ConsoleColor darkCyan = ConsoleColor.DarkCyan;
+        public static ConsoleColor cyan = ConsoleColor.Cyan;
+    }
+    public class RGB_COLORS
+    {
+        public static ConsoleColor r = ConsoleColor.Red;
+        public static ConsoleColor g = ConsoleColor.Green;
+        public static ConsoleColor b = ConsoleColor.Blue;
+    }
+    
+}
